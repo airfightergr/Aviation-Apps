3c9f549913a707041d687ed35ffed024333f515b
airfightergr <airfightergr@yahoo.com>
2019-02-17 20:24:32 +0200

    Updated main menu layout.

--- menu.lua
+++ menu.lua
@@ -4,6 +4,10 @@ local ltn12 = require( "ltn12" )
 local helpers = require( "helpers" )
 local composer = require( "composer" )
 --local udp = assert(socket.udp())
+local backgroundTex = graphics.newTexture({type = 'image', filename = 'assets/background.png'})
+local buttImageSheetOpts = {width = 512, height = 512, numFrames = 4, sheetContentWidth = 2048, sheetContentHeight = 512}
+local buttImageSheet = graphics.newImageSheet("assets/menu_icons.png", buttImageSheetOpts)
+
 
 local scene = composer.newScene()
 
@@ -11,16 +15,6 @@ local scene = composer.newScene()
 -- Code outside of the scene event functions below will only be executed ONCE unless
 -- the scene is removed entirely (not recycled) via "composer.removeScene()"
 -- -----------------------------------------------------------------------------------
-local bg
-local title
-local buttonConversions
-local buttonConversionsLabel
-local buttonWeather
-local buttonWeatherLabel
-local buttonComputations
-local buttonComputationsLabel
-local buttonMovingMap
-local buttonMovingMapLabel
 
 -- local function changeScenes()
 -- composer.gotoScene( "weather", {effect = "slideLeft", time = 500} )
@@ -58,90 +52,70 @@ function scene:create( event )
 
 	local sceneGroup = self.view
 	-- Code here runs when the scene is first created but has not yet appeared on screen
-	bg = display.newRect( display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight )
-	bg:setFillColor(1,1,1)
-	sceneGroup:insert(bg)
-	local image = display.newImage( "assets/logo.png" , display.contentCenterX, display.contentCenterY*1.8)
+	local background = display.newImageRect( backgroundTex.filename, backgroundTex.baseDir, display.contentWidth, display.contentHeight )
+	background.x = display.contentCenterX
+	background.y = display.contentCenterY
+	sceneGroup:insert(background)
+	local image = display.newImage( "assets/logo.png" , display.contentCenterX, display.contentHeight*0.75)
 	sceneGroup:insert(image)
-	title = display.newText("Aviator's Companion", display.contentCenterX, display.contentHeight*0.075,native.newFont( "Helvetica" , 40 ))
-	title:setFillColor(0,0,0)
+	title = display.newText("AVIATOR'S COMPANION", display.contentCenterX, display.contentHeight*0.075,native.newFont( "FMS3000.ttf" , 50 ))
+	title:setFillColor(1,1,1)
 	sceneGroup:insert(title)
-
-	--------------------------------------------------------------------------------
-	--Button Conversions
-	--------------------------------------------------------------------------------
-	buttonConversions = display.newRoundedRect(display.contentCenterX, display.contentHeight*0.2,
-											display.contentWidth*0.5, display.contentHeight*0.075, 15 )
-	buttonConversions:setFillColor(0.3,0.5,0.3)
-	sceneGroup:insert(buttonConversions)
-
-	buttonConversions:addEventListener("tap", gotoConversions)
-
-	buttonConversionsLabel = display.newText( "Conversions",  display.contentCenterX, display.contentHeight*0.2, native.newFont( "Helvetica" ,30 ))
-	sceneGroup:insert(buttonConversionsLabel)
 
 	--------------------------------------------------------------------------------
 	--Button Weather
 	--------------------------------------------------------------------------------
-	buttonWeather = display.newRoundedRect(display.contentCenterX, display.contentHeight*0.3,
-											display.contentWidth*0.5, display.contentHeight*0.075, 15 )
-	buttonWeather:setFillColor(0.3,0.3,0.5)
-	sceneGroup:insert(buttonWeather)
+	local buttWeather = display.newImageRect( buttImageSheet, 1, display.contentWidth*0.35, display.contentWidth*0.35)
+	buttWeather.x = display.contentWidth * 0.25
+	buttWeather.y = display.contentHeight * 0.25
+	sceneGroup:insert(buttWeather)
+	buttWeather:addEventListener("tap", gotoWeather)
 
-	buttonWeatherLabel = display.newText( "Weather",  display.contentCenterX, display.contentHeight*0.3, native.newFont( "Helvetica" ,30 ))
-	sceneGroup:insert(buttonWeatherLabel)
-	buttonWeather:addEventListener("tap", gotoWeather)
+	--------------------------------------------------------------------------------
+	--Button Conversions
+	--------------------------------------------------------------------------------
+	local buttConv = display.newImageRect( buttImageSheet, 2 , display.contentWidth*0.35, display.contentWidth*0.35)
+	buttConv.x = display.contentWidth * 0.75
+	buttConv.y = display.contentHeight * 0.25
+	sceneGroup:insert(buttConv)
+	buttConv:addEventListener("tap", gotoConversions)
 
 	--------------------------------------------------------------------------------
 	--Button Computations
 	--------------------------------------------------------------------------------
-	buttonComputations = display.newRoundedRect(display.contentCenterX, display.contentHeight*0.4,
-											display.contentWidth*0.5, display.contentHeight*0.075, 15 )
-	buttonComputations:setFillColor(0.3,0.5,0.6)
-	sceneGroup:insert(buttonComputations)
-
-	buttonComputationsLabel = display.newText( "Computations",  display.contentCenterX, display.contentHeight*0.4, native.newFont( "Helvetica" ,30 ))
-	sceneGroup:insert(buttonComputationsLabel)
-	buttonComputations:addEventListener("tap", gotoComputations)
-
+	local buttCompute = display.newImageRect( buttImageSheet, 3 , display.contentWidth*0.35, display.contentWidth*0.35)
+	buttCompute.x = display.contentWidth * 0.25
+	buttCompute.y = display.contentHeight * 0.50
+	sceneGroup:insert(buttCompute)
+	buttCompute:addEventListener("tap", gotoComputations)
 	--------------------------------------------------------------------------------
-	--Button MovingMap
+	--Button Nothing!
 	--------------------------------------------------------------------------------
-	buttonMovingMap = display.newRoundedRect(display.contentCenterX, display.contentHeight*0.5,
-											display.contentWidth*0.5, display.contentHeight*0.075, 15 )
-	buttonMovingMap:setFillColor(0.5,0.3,0.3)
-	sceneGroup:insert(buttonMovingMap)
-
-	buttonMovingMapLabel = display.newText( "Moving Map",  display.contentCenterX, display.contentHeight*0.5, native.newFont( "Helvetica" ,30 ))
-	sceneGroup:insert(buttonMovingMapLabel)
-	buttonMovingMap:addEventListener("tap", gotoMovingMap)
+	local buttLast = display.newImageRect( buttImageSheet, 4 , display.contentWidth*0.35, display.contentWidth*0.35)
+	buttLast.x = display.contentWidth * 0.75
+	buttLast.y = display.contentHeight * 0.50
+	sceneGroup:insert(buttLast)
+	--buttLast:addEventListener("tap", gotoComputations)
 
 	--------------------------------------------------------------------------------
 	--Button Manual
 	--------------------------------------------------------------------------------
-	buttonManual = display.newRoundedRect(display.contentCenterX, display.contentHeight*0.7,
-											display.contentWidth*0.5, display.contentHeight*0.075, 15 )
+	buttonManual = display.newRoundedRect(display.contentCenterX, display.contentHeight*0.67,
+											display.contentWidth*0.35, display.contentHeight*0.05, 15 )
 	buttonManual:setFillColor(0.9,0.9,0.0)
 	sceneGroup:insert(buttonManual)
 
-	buttonManualLabel = display.newText( "Open Manual",  display.contentCenterX, display.contentHeight*0.7, native.newFont( "Helvetica" ,30 ))
+	buttonManualLabel = display.newText( "Read Manual",  display.contentCenterX, display.contentHeight*0.67, native.newFont( "Helvetica" ,25 ))
 	buttonManualLabel:setFillColor(0,0,0)
 	sceneGroup:insert(buttonManualLabel)
 	buttonManualLabel:addEventListener("tap", openWiki)
 --------------------------------------------------------------------------------
 --TESTS
 --------------------------------------------------------------------------------
+local bottomTitle = display.newText( "Do not use in Real Life Aviation", display.contentCenterX, display.contentHeight * 0.83, native.newFont( "Helvetica" , 20 ))
+	bottomTitle:setFillColor(1,0.2,0.2)
+sceneGroup:insert(bottomTitle)
 
-
-local IPDisplay_title = display.newText( "X-Plane Network Settings", display.contentCenterX, display.contentHeight*0.77,
-												native.newFont( "Helvetica" , 20 ))
-			IPDisplay_title:setFillColor(0,0,0)
-sceneGroup:insert(IPDisplay_title)
-
-local IPDisplay_add = display.newText( string.format("IP address: %s - Port: 49003", globals.deviceIP ), display.contentCenterX, display.contentHeight*0.8,
-												native.newFont( "Helvetica" , 20 ))
-			IPDisplay_add:setFillColor(0,0,0)
-sceneGroup:insert(IPDisplay_add)
 
 
 
